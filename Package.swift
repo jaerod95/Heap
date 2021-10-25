fc865319fcc5b565379a4fee5ba2774f949b968b
Jason Rodriguez <jrodriguez@simplenexus.com>
2021-10-25 15:48:59 -0700

    Adds Heap

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "Heap",
+    platforms: [
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Heap",
+            targets: ["HeapFramework"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        .binaryTarget(
+            name: "HeapFramework",
+            path: "Heap.xcframework"
+        )
+    ]
+)
